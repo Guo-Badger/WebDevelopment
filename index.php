045fca3fe656b863790830e30ac43afb7af6c9da
Guo Yu <yuguo365@gmail.com>
2017-05-22 14:55:14 -0500

    updates

--- index.php
+++ index.php
@@ -8,65 +8,96 @@
   		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
-
 	</head>
 	
 
 
 	<body>
-		<header id="navbar" class="header-nav" role="banner">
-			<div class="header-container">
-				<div class="row">
-					<div class="logo-header-container">
-						<a class="logo logo-header" href="/" title="Home">HydroExtremes</a>
-					</div>
-					<div class="leaf">
-						<a href="http://10.141.89.2/WebDevelopment/"><b>Home</b></a>
-					</div>
-					<div class="leaf">
-						<a href="http://10.141.89.2/WebDevelopment/groupmembers"><b>Group Members</b></a>
-					</div>
-					<div class="leaf">
-						<a href="http://10.141.89.2/WebDevelopment/projects" data-link="Projects"><b>Projects</b></a>
-					</div>
-					<div class="leaf">
-						<a href="http://10.141.89.2/WebDevelopment/publications"><b>Publications</b></a>
-					</div>
-					<div class="leaf">
-						<a href="http://10.141.89.2/WebDevelopment/collaborators"><b>Collaborators</b></a>
-					</div>
-
+		<header class="navbar navbar-inverse">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+           	 		</button>
+            		<a class="navbar-brand" href="#">Hydroclimate Extremes Research Group</a>
 				</div>
 
-				<div class="header-menu-dropdown">
-					<ul>
-						<li>RainyDay</li>
-						<li>Regional Landslide Modeling</li>
-						<li>Tropical Cyclones, Flooding, and the Regional Hydroclimate</li>
-						<li>Rainfall remote sensing for hydrology</li>
-						<li>Urbanization and Hydrologic Change</li>
+				<!-- Navbar Links -->
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="http://10.141.89.2/WebDevelopment/"><b>Home</b></a></li>
+						<li><a href="http://10.141.89.2/WebDevelopment/groupmembers"><b>Group Members</b></a></li>
+						      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><b>Projects</b><span class="caret"></span></a>
+						        <ul class="dropdown-menu">
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/rainyday">RainyDay: Rainfall for Modern Flood Hazard Assessment</a></li>
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/landslide-modelling">Regional Landslide Modeling</a></li>
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/tropical-cyclones">Tropical Cyclones, Flooding, and the Regional Hydroclimate</a></li>
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/remote-sensing">Rainfall remote sensing for hydrology</a></li>
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/urbanization">Urbanization and Hydrologic Change</a></li>
+						        </ul>
+						      </li>
+						<li><a href="http://10.141.89.2/WebDevelopment/publications"><b>Publications</b></a></li>
+						<li><a href="http://10.141.89.2/WebDevelopment/collaborators"><b>Collaborators</b></a></li>
 					</ul>
 				</div>
 
 			</div>
 		</header>
 		
-		<header class="container">
-			<h1 class="page-header">Hydroclimate Extremes Research Group</h1>
-		</header>
-		
-		<div class="main-container">
-			<div class="carousel-homepage">
-				
-			</div>
-			<div class="sub-container">
-				
-			</div>
-			<div class="home-news">
-				
-			</div>
+		<!-- Navbar Infomation -->
+		<div class="container">
+			<p class="navabr navbar-info">Hydroclimate Extremes Research Group</p>
 		</div>
+		
+		<!-- Background Image -->
+		<div id="myCarousel" class="carousel slide" data-ride="carousel">
+    		<!-- Indicators -->
+		    <ol class="carousel-indicators">
+		      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		      <li data-target="#myCarousel" data-slide-to="1"></li>
+		      <li data-target="#myCarousel" data-slide-to="2"></li>
+		    </ol>
+
+		    <!-- Wrapper for slides -->
+		    <div class="carousel-inner">
+		      <div class="item active">
+		        <img src="images/home-floodrisk.jpg" alt="Flood Risk" style="width:100%;">
+		        <div class="carousel-caption">
+		        	<h3>Reducing Flood Risks</h3>
+		        	<p>An ounce of prevention is worth a pound of cure</p>
+		        </div>
+		      </div>
+
+		      <div class="item">
+		        <img src="images/home-gpm.jpg" alt="GPM" style="width:100%;">
+		        <div class="carousel-caption">
+		        	<h3>NASA-GPM</h3>
+		        	<p>A Changing Data Environment</p>
+		        </div>
+		      </div>
+		    
+		      <div class="item">
+		        <img src="images/home-opensource.png" alt="Open Source" style="width:100%;">
+		        <div class="carousel-caption">
+		        	<h3>Rainy Day</h3>
+		        	<p></p>
+		        </div>
+		      </div>
+		    </div>
+
+		    <!-- Left and right controls -->
+		    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+		      <span class="glyphicon glyphicon-chevron-left"></span>
+		      <span class="sr-only">Previous</span>
+		    </a>
+		    <a class="right carousel-control" href="#myCarousel" data-slide="next">
+		      <span class="glyphicon glyphicon-chevron-right"></span>
+		      <span class="sr-only">Next</span>
+		    </a>
+  		</div>
+
 
 
 
