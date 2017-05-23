d9c1f30ae6628c0837ef35cedcce7897a9110e85
Guo Yu <yuguo365@gmail.com>
2017-05-22 20:23:50 -0500

    updates

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
   		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  		<link rel="stylesheet" type="text/css" href="css/custom.css">
 	</head>
 	
 
@@ -48,7 +49,7 @@
 		
 		<!-- Navbar Infomation -->
 		<div class="container">
-			<p class="navabr navbar-info">Hydroclimate Extremes Research Group</p>
+			<p class="navabr-navbar-info">Hydroclimate Extremes Research Group</p>
 		</div>
 		
 		<!-- Background Image -->
@@ -81,7 +82,7 @@
 		      <div class="item">
 		        <img src="images/home-opensource.png" alt="Open Source" style="width:100%;">
 		        <div class="carousel-caption">
-		        	<h3>Rainy Day</h3>
+		        	<h3></h3>
 		        	<p></p>
 		        </div>
 		      </div>
@@ -98,15 +99,73 @@
 		    </a>
   		</div>
 
+		<!-- Homepage Maincontent -->
+		<div class="container">
+			<div class="maincontent">
+				<p><span class="firstword">Welcome</span> to the homepage of the Hydroclimate Extremes Research Group, part of the Civil and Environmental Engineering Department at the University of Wisconsin. We work in a number of research areas, including:</p>
+				<ul>
+					<li>Measurement, modeling, and analysis of extreme rainfall at high resolution using a variety of sources including rain gages, ground-based weather radar, satellite-based sensors, and numerical weather and climate models.</li>
+					<br>
+					<li>Understanding the role of rainfall and soil moisture variability and structure and their interactions with watershed properties to produce floods at a wide range of scales using high-resolution multi-scale supercomputer-based watershed models.</li>
+					<br>
+					<li>Interfacing with meteorologists and climate scientists to translate projected changes in extreme precipitation from regional and global climate models into projections of future risks.</li>
+					<br>
+					<li>Developing practical tools for improved probabilistic flood hazard and risk estimation, including in urban areas, ungaged watersheds, and developing countries.</li>
+				</ul>
+			</div>
 
+			<div class="maincontent">
+				<p><span class="firstword">In addition,</span> we plan to expand our expertise into related areas, including:</p>
+				<ul>
+					<li>Understanding the role of rainfall variability and structure as a driver of watershed functions including sediment movement and water quality using high-resolution observations, multi-scale models, and probabilistic methods.</li>
+					<br>
+					<li>Estimating the economic and social impacts of current and future flood threats, as well as the benefits of financial and infrastructure-based risk management strategies.</li>
+					<br>
+					<li>Interdisciplinary assessment of multiple hazards posed by tropical cyclones in the present and future climate.</li>
+					<br>
+					<li>Monitoring and projecting changes in rivers, lakes, and wetlands through the fusion of satellite data with ground-based observations and models.</li>
+				</ul>
+				<p>We invite discussion and collaboration on these topics and any others.  Those interested in undergraduate, graduate, and post-graduate research opportunities should feel free to contact us about possible openings.  Our studies, datasets, and codes can be made available upon request.</p>
+			</div>
+			<div class="maincontent">
+				<p><span class="firstword">NOTE:</span> We are currently looking for talented undergraduate, masters, and PhD students.  All are welcome to apply but those with strong backgrounds in hydrology or atmospheric sciences are particularly encouraged. Experience in computer programming and data analysis, or a keen willingness to learn these skills, is critical.</p>
+				<br>
+				<p>Please contact Dr. Wright if you are interested in applying or would like to find more information.</p>
+			</div>
 
-
-
-
+		</div>
 	</body>
 
+	<footer>
+		<div class="collapse navbar-collapse">
+		<div class="container">
+			<div class="col">
+				<img src="http://10.141.89.2/WebDevelopment/images/home-uwlogo.png" style=" padding: 15px">
+			</div>
+			
+			<div class="col">
+				<h3>CONTACT</h3>
+				<p>Room: 1269C Engineering Hall 1415 Engineering Drive Madison, WI 53706</p>
+				<p>Ph: (608) 262-1978</p>
+				<p><a href="mailto:danielb.wright@wisc.edu">danielb.wright@wisc.edu</a></p>
+			</div>
+			
+			<div class="col" >
+				<h3>LINKS</h3>
+				<ul class="footer-ul">
+		          <li><a href="https://www.engr.wisc.edu/department/civil-environmental-engineering/"> UW-Madison: Civil & Environmental Engineering</a></li>
+		          <li><a href="http://www.usgs.gov/"> U.S. Geological Survey</a></li>
+		          <li><a href="http://www.usbr.gov/"> U.S. Bureau of Reclamation</a></li>
+		          <li><a href="http://www.nasa.gov/"> NASA</a></li>
+		          <li><a href="http://www.noaa.gov/"> National Oceanic & Atmospheric Administration</a></li>
+		          <li><a href="https://www.nsf.gov/"> National Science Foundation</a></li>
+		        </ul>
+			</div>
+		</div>
+		</div>
+		<div class="copyright">
+			<p>© 2017 University of Wisconsin Madison</p>
+		</div>
+	</footer>
 
-
-
-	<footer></footer>
 </html>
--- projects/rainyday/index.php
+++ projects/rainyday/index.php
@@ -4,68 +4,104 @@
 
 <html>
 	<head>
-		
-
-
-
+		<title>Hydroclimate Extremes Research Group</title>
+  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  		<link rel="stylesheet" type="text/css" href="css/custom.css">
 	</head>
 	
 
 
 	<body>
-		<header id="navbar" class="header-nav" role="banner">
-			<div class="header-container">
-				<div class="row">
-					<div class="logo-header-container">
-						<a class="logo logo-header" href="/" title="Home">HydroExtremes</a>
-					</div>
-					<div class="leaf">
-						<a href="http://10.141.89.2/WebDevelopment/"><b>Home</b></a>
-					</div>
-					<div class="leaf">
-						<a href="http://10.141.89.2/WebDevelopment/groupmembers"><b>Group Members</b></a>
-					</div>
-					<div class="leaf">
-						<a href="http://10.141.89.2/WebDevelopment/projects" data-link="Projects"><b>Projects</b></a>
-					</div>
-					<div class="leaf">
-						<a href="http://10.141.89.2/WebDevelopment/publications"><b>Publications</b></a>
-					</div>
-					<div class="leaf">
-						<a href="http://10.141.89.2/WebDevelopment/collaborators"><b>Collaborators</b></a>
-					</div>
-
+		<header class="navbar navbar-inverse">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+           	 		</button>
+            		<a class="navbar-brand" href="#">Hydroclimate Extremes Research Group</a>
 				</div>
 
-				<div class="header-menu-dropdown">
-					<ul>
-						<li>RainyDay</li>
-						<li>Regional Landslide Modeling</li>
-						<li>Tropical Cyclones, Flooding, and the Regional Hydroclimate</li>
-						<li>Rainfall remote sensing for hydrology</li>
-						<li>Urbanization and Hydrologic Change</li>
+				<!-- Navbar Links -->
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="http://10.141.89.2/WebDevelopment/"><b>Home</b></a></li>
+						<li><a href="http://10.141.89.2/WebDevelopment/groupmembers"><b>Group Members</b></a></li>
+						      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><b>Projects</b><span class="caret"></span></a>
+						        <ul class="dropdown-menu">
+						          <li><a href="#">RainyDay: Rainfall for Modern Flood Hazard Assessment</a></li>
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/landslide-modelling">Regional Landslide Modeling</a></li>
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/tropical-cyclones">Tropical Cyclones, Flooding, and the Regional Hydroclimate</a></li>
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/remote-sensing">Rainfall remote sensing for hydrology</a></li>
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/urbanization">Urbanization and Hydrologic Change</a></li>
+						        </ul>
+						      </li>
+						<li><a href="http://10.141.89.2/WebDevelopment/publications"><b>Publications</b></a></li>
+						<li><a href="http://10.141.89.2/WebDevelopment/collaborators"><b>Collaborators</b></a></li>
 					</ul>
 				</div>
 
 			</div>
 		</header>
 		
-		<header class="container">
-			<h1 class="page-header">Hydroclimate Extremes Research Group</h1>
-		</header>
-		
-		<div class="main-container">
-			
+		<!-- Navbar Infomation -->
+		<div class="container">
+			<p class="navabr-navbar-info">Hydroclimate Extremes Research Group</p>
 		</div>
+		
+	
 
-
-
-
+		<!-- Homepage Maincontent -->
+		<div class="container">
+			<div class="maincontent">
+				<p><span class="firstword">Welcome</span> to the homepage of the Hydroclimate Extremes Research Group, part of the Civil and Environmental Engineering Department at the University of Wisconsin. We work in a number of research areas, including:</p>
+				<ul>
+					<li>Measurement, modeling, and analysis of extreme rainfall at high resolution using a variety of sources including rain gages, ground-based weather radar, satellite-based sensors, and numerical weather and climate models.</li>
+					<br>
+					<li>Understanding the role of rainfall and soil moisture variability and structure and their interactions with watershed properties to produce floods at a wide range of scales using high-resolution multi-scale supercomputer-based watershed models.</li>
+					<br>
+					<li>Interfacing with meteorologists and climate scientists to translate projected changes in extreme precipitation from regional and global climate models into projections of future risks.</li>
+					<br>
+					<li>Developing practical tools for improved probabilistic flood hazard and risk estimation, including in urban areas, ungaged watersheds, and developing countries.</li>
+				</ul>
+			</div>
+		</div>
 
 	</body>
 
+	<footer>
+		<div class="collapse navbar-collapse">
+		<div class="container">
+			<div class="col">
+				<img src="http://10.141.89.2/WebDevelopment/images/home-uwlogo.png" style=" padding: 15px">
+			</div>
+			
+			<div class="col">
+				<h3>CONTACT</h3>
+				<p>Room: 1269C Engineering Hall 1415 Engineering Drive Madison, WI 53706</p>
+				<p>Ph: (608) 262-1978</p>
+				<p><a href="mailto:danielb.wright@wisc.edu">danielb.wright@wisc.edu</a></p>
+			</div>
+			
+			<div class="col" >
+				<h3>LINKS</h3>
+				<ul class="footer-ul">
+		          <li><a href="https://www.engr.wisc.edu/department/civil-environmental-engineering/"> UW-Madison: Civil & Environmental Engineering</a></li>
+		          <li><a href="http://www.usgs.gov/"> U.S. Geological Survey</a></li>
+		          <li><a href="http://www.usbr.gov/"> U.S. Bureau of Reclamation</a></li>
+		          <li><a href="http://www.nasa.gov/"> NASA</a></li>
+		          <li><a href="http://www.noaa.gov/"> National Oceanic & Atmospheric Administration</a></li>
+		          <li><a href="https://www.nsf.gov/"> National Science Foundation</a></li>
+		        </ul>
+			</div>
+		</div>
+		</div>
+		<div class="copyright">
+			<p>© 2017 University of Wisconsin Madison</p>
+		</div>
+	</footer>
 
-
-
-	<footer></footer>
 </html>
