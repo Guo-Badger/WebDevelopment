234c191c88a6def8d30ccabc81ee3e817d7f4656
Guo Yu <yuguo365@gmail.com>
2017-05-31 10:42:18 -0500

    git

---
+++ groupmembers/index.php
@@ -0,0 +1,227 @@
+
+
+
+
+<html>
+	<head>
+		<title>Hydroclimate Extremes Research Group</title>
+  		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  		<link rel="stylesheet" type="text/css" href="../css/custom.css">
+	</head>
+	
+
+
+	<body>
+		<header class="navbar navbar-inverse">
+			<div class="container">
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+		                <span class="icon-bar"></span>
+           	 		</button>
+            		<a class="navbar-brand" href="http://10.141.89.2/WebDevelopment/">Hydroclimate Extremes Research Group</a>
+				</div>
+
+				<!-- Navbar Links -->
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav">
+						<li><a href="http://10.141.89.2/WebDevelopment/"><b>Home</b></a></li>
+						<li class="active"><a href="http://10.141.89.2/WebDevelopment/groupmembers"><b>Group Members</b></a></li>
+						      <li class="dropdown"><a class="dropdown-toggle" data-toggle="dropdown" href="#"><b>Projects</b><span class="caret"></span></a>
+						        <ul class="dropdown-menu">
+						          <li><a href="#">RainyDay: Rainfall for Modern Flood Hazard Assessment</a></li>
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/landslide-modelling">Regional Landslide Modeling</a></li>
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/tropical-cyclones">Tropical Cyclones, Flooding, and the Regional Hydroclimate</a></li>
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/remote-sensing">Rainfall remote sensing for hydrology</a></li>
+						          <li><a href="http://10.141.89.2/WebDevelopment/projects/urbanization">Urbanization and Hydrologic Change</a></li>
+						        </ul>
+						      </li>
+						<li><a href="http://10.141.89.2/WebDevelopment/publications"><b>Publications</b></a></li>
+						<li><a href="http://10.141.89.2/WebDevelopment/collaborators"><b>Collaborators</b></a></li>
+					</ul>
+				</div>
+
+			</div>
+		</header>
+		
+				<!-- Navbar Infomation -->
+		<div class="container">
+			<p class="navabr-navbar-info">Group Members</p>
+		</div>
+		
+		<br>
+		<!-- Group Members -->
+		<div class="container">
+
+			<div class="team-alignleft">
+				<div>
+					<h1>Welcome to the Hydroclimate Extremes Research Group</h1>
+				</div>
+
+			</div>
+
+
+			<div class="team-alignleft">
+				<div>
+					<img class="teamimage" src="http://10.141.89.2/WebDevelopment/images/team/DanielWright.jpg">
+				</div>
+				<div class="text-box">
+					<h2>Daniel Wright, PhD</h2>
+					<p> <strong>Assistant Professor</strong> </p>
+		
+					<h4>Education:</h4>
+					<p>PhD, Princeton University, 2009-2013<br />
+					   MSE, University of Michigan, 2005<br />
+					   BSE, University of Michigan, 2001-2005</p>
+
+					<h4>Contact:</h4>
+					<p>
+						Email: <a href= "mailto:danielb.wright@wisc.edu">danielb.wright@wisc.edu</a><br />
+						Phone: 608.262.1978
+					 </p>
+					
+					<h4>Research Interests:</h4>
+					<p>Satellite and ground-based rainfall remote sensing; rainfall and flood processes at urban and regional scales; computationally-intensive simulation for probabilistic natural hazards risk assessment; statistical and stochastic hydrology; modernization of stormwater and flood management theory and practice.</p>
+
+
+					<h4>Bio:</h4>
+					<p>Before joining the CEE faculty at the University of Wisconsin-Madison, Dr. Wright was a NASA Postdoctoral Program fellow in the Hydrological Sciences Lab at Goddard Space Flight Center in Greenbelt, Maryland.  He holds bachelors and masters degrees in Civil and Environmental Engineering from the University of Michigan.  Daniel served a Regional Sanitation Engineer with the Peace Corps in Bolivia from 2006-2008 and worked as a consulting hydropower engineer in Chile from 2008-2009 before earning his Ph.D. in Environmental Engineering and Water Resources from Princeton University, where he studied urban rainfall and flood hydrometeorology with Dr. James Smith from 2009 to 2013.  He worked as a disaster risk management consultant at the World Bank from 2013-2014, focusing on flood and landslide risk management in Latin America and the Caribbean. </p>
+				</div>
+			</div>
+
+			<div class="team-alignright">
+				<div>
+					<img class="teamimage" src="http://10.141.89.2/WebDevelopment/images/team/ChrisBosma.jpg">
+				</div>
+				<div class="text-box">
+					<h2>Chris Bosma</h2>
+					<p> <strong>PhD Pre-Candidate</strong> </p>
+		
+					<h4>Education:</h4>
+					<p>BS, Georgia Institute of Technology, 2015</p>
+
+					<h4>Contact:</h4>
+					<p>
+						Email: <a href= "mailto:cbosma@wisc.edu">cbosma@wisc.edu</a><br />
+						Phone: 770.870.8001
+					 </p>
+					
+					<h4>Bio:</h4>
+					<p> Christopher comes to the University of Wisconsin-Madison from Atlanta, GA, where he previously served as a research technician and lab manager for Dr. Kim Cobb’s paleoclimate and climate change laboratory at the Georgia Institute of Technology. His previous research utilized stable water isotope measurements to analyze precipitation trends and variability at the lab’s field site in Gunung Mulu National Park on the island of Borneo. Christopher also served as a student volunteer at the local National Weather Service forecast office in Peachtree City, GA, where he developed a twenty-five-year climatological survey of roadway “black ice” events in the Atlanta metropolitan area. He is a 2015 graduate of the School of Earth and Atmospheric Sciences at Georgia Tech. </p>
+				</div>
+			</div>
+			<div class="team-alignleft">
+				<div>
+					<img class="teamimage" src="http://10.141.89.2/WebDevelopment/images/team/GuoYu.jpg">
+				</div>
+				<div class="text-box">
+					<h2>Guo Yu</h2>
+					<p> <strong>PhD pre-candidate</strong> </p>
+		
+					<h4>Education:</h4>
+					<p>
+					   MSC, EuroAquae-ERASMUS MUNDUS JOINT MASTER DEGREES, 2011-2013<br />
+					   BSE, Tianjin Chengjian University, 2007-2011</p>
+
+					<h4>Contact:</h4>
+					<p>
+						Email: <a href= "mailto:danielb.wright@wisc.edu">gyu29@wisc.edu</a><br />
+						Phone: 607.280.3999
+					 </p>
+					
+
+					<h4>Bio:</h4>
+					<p>Guo received a bachelor degree in Water Supply and Drainage Engineering in 2011 from Tianjin Chengjian University, and a joint MS in Hydroinformatics in 2013 from Newcastle University (United Kingdom), Polytech Nice (France) and Brandenburg University of Technology (Germany). He was a Hydraulic Engineer and Team Leader in DHI China, 2013-2016.
+					 <br />
+					Guo is interested in studying and developing hydrological, hydrodynamic, and water quality models and how to apply these modelling technologies and analytics to solve a challenge in flood hazards control, urban stormwater management and environmental fluid mechanics.
+					</p>
+				</div>
+			</div>
+
+			<div class="team-alignright">
+				<div>
+					<img class="teamimage" src="http://10.141.89.2/WebDevelopment/images/team/AlexaSampson.png">
+				</div>
+				<div class="text-box">
+					<h2>Alexa Sampson</h2>
+					<p> <strong>Master</strong> </p>
+		
+					<h4>Education:</h4>
+					<p>University of Wisconsin-Madison, 2015</p>
+
+					<h4>Contact:</h4>
+					<p>
+						Email: <a href= "mailto:aasampson@wisc.edu">aasampson@wisc.edu</a><br />
+						Phone: 608.780.4068
+					 </p>
+					
+					<h4>Bio:</h4>
+					<p> Alexa received her undergraduate degree in Civil Engineering at the University of Wisconsin-Madison in 2015. She is now pursuing a Master’s degree in the same field with a focus on Water Resources Engineering. During her undergraduate career Alexa’s research involved modeling daily variations in temperature and oxygen profiles in a dimictic lake as driven by atmospheric data. She used this to examine how a changing climate could cause temperature and oxygen stress for coldwater fish. As an intern at the Wisconsin Department of Transportation, Alexa worked to update and improve a comprehensive atlas identifying sections of the Wisconsin interstates that are at risk of flooding during a 100-year event. Today she researches the effects of rainfall rate averaging on runoff estimation using a one-dimensional infiltration model. </p>
+				</div>
+			</div>
+
+			<div class="team-alignleft">
+				<div>
+					<img class="teamimage" src="http://10.141.89.2/WebDevelopment/images/team/AllisonLobue.jpg">
+				</div>
+				<div class="text-box">
+					<h2>Allison Lobue</h2>
+					<p> <strong>Undergraduate</strong> </p>
+		
+					<h4>Education:</h4>
+					<p>
+					   BS, University of Wisconsin-Madison
+					</p>
+
+					<h4>Contact:</h4>
+					<p>
+						Email: <a href= "mailto:alobue@wisc.edu">alobue@wisc.edu</a><br />
+					 </p>
+					
+
+					<h4>Bio:</h4>
+					<p>Allison is pursuing her undergraduate degree in Biological Systems Engineering with a focus in Natural Resources and Environmental Engineering at the University of Wisconsin-Madison. She has undergraduate research experience with the Hydroecology Research Group and the Water, Sustainability, and Climate Research Project, focusing on designing and implementing a plant root density imaging device and statistical modeling of hydrologic, soil, and plant relationships to determine the connection between soil moisture and plant root distribution in multiple ecosystems. Allison is also a stream monitor volunteer with the Rock River Coalition and collects data on temperature, dissolved oxygen, water clarity, and aquatic species in the Madison, Wisconsin area.
+					</p>
+				</div>
+			</div>
+		  
+		</div>
+	
+	</body>
+
+	<footer>
+		<div class="collapse navbar-collapse">
+		<div class="container">
+			<div class="col">
+				<img src="http://10.141.89.2/WebDevelopment/images/home-uwlogo.png" style=" padding: 15px">
+			</div>
+			
+			<div class="col">
+				<h3>CONTACT</h3>
+				<p>Room: 1269C Engineering Hall 1415 Engineering Drive Madison, WI 53706</p>
+				<p>Ph: (608) 262-1978</p>
+				<p><a href="mailto:danielb.wright@wisc.edu">danielb.wright@wisc.edu</a></p>
+			</div>
+			
+			<div class="col" >
+				<h3>LINKS</h3>
+				<ul class="footer-ul">
+		          <li><a href="https://www.engr.wisc.edu/department/civil-environmental-engineering/"> UW-Madison: Civil & Environmental Engineering</a></li>
+		          <li><a href="http://www.usgs.gov/"> U.S. Geological Survey</a></li>
+		          <li><a href="http://www.usbr.gov/"> U.S. Bureau of Reclamation</a></li>
+		          <li><a href="http://www.nasa.gov/"> NASA</a></li>
+		          <li><a href="http://www.noaa.gov/"> National Oceanic & Atmospheric Administration</a></li>
+		          <li><a href="https://www.nsf.gov/"> National Science Foundation</a></li>
+		        </ul>
+			</div>
+		</div>
+		</div>
+		<div class="copyright">
+			<p>© 2017 University of Wisconsin Madison</p>
+		</div>
+	</footer>
+
+</html>
