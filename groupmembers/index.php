ca88ebcdbbe0b510dd61afc78f31d5bb0649dae7
Guo Yu <yuguo365@gmail.com>
2017-05-31 10:59:55 -0500

    1

--- groupmembers/index.php
+++ groupmembers/index.php
@@ -8,7 +8,7 @@
   		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
   		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
   		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-  		<link rel="stylesheet" type="text/css" href="http://10.141.89.2/WebDevelopment/css/custom.css">
+  		<link rel="stylesheet" type="text/css" href="../css/custom.css">
 	</head>
 	
 
